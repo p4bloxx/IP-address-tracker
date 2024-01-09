97e517ab533af5a29d7377982c7ad24a52a1197c
Pablo Dettori <106544349+p4bloxx@users.noreply.github.com>
2024-01-09 11:36:45 +0100

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,90 @@
+const currentIp = document.querySelector('#ip-number')
+const adress = document.querySelector('#location')
+const timeOffset = document.querySelector('#time-offset')
+const provider = document.querySelector('#isp')
+const sendIp = document.getElementById('ip');
+const submit = document.getElementById('submit');
+
+function startIp(userIp, newDomain){
+
+  const getIp = async () => {
+    try{
+      const apiAdress = await fetch(`https://geo.ipify.org/api/v2/country,city?apiKey=at_dR7YxR8WrSTTKIaAabjwsnqZLZQKA&ipAddress=${userIp}&domain=${newDomain}`)
+  
+      if(!apiAdress.ok){
+        if(apiAdress.status === 400){
+          throw new Error ('Data not found');
+        } else if(apiAdress.status === 500){
+          throw new Error ('problem of server');
+        } else {
+          throw new Error ('Network response was not ok')
+        }
+      }
+  
+      const responseJson = await apiAdress.json();
+      const data = responseJson;
+      const ipDefault = data.ip;
+      const cityDefault = data.location.city;
+      const regionDefault = data.location.region;
+      const timeZoneDefault = data.location.timezone;
+      let latDefault = data.location.lat;
+      let lngDefault = data.location.lng;
+      const ispDefault = data.isp;
+
+      
+      let blackIcon = L.icon({
+        iconUrl: 'blackIcon.png',
+        iconAnchor:   [22, 94],
+        shadowAnchor: [4, 62],
+        popupAnchor:  [-3, -76]
+      });
+      
+      let container = L.map('map').setView([latDefault, lngDefault], 13);
+      let marker = L.marker([latDefault, lngDefault], {icon: blackIcon}).addTo(container);
+    
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+          maxZoom: 19,
+          attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      }).addTo(container);
+           
+      sendCoordinate(container);
+
+      currentIp.innerText = ipDefault;
+      adress.innerText = `${cityDefault}, ${regionDefault}`;
+      timeOffset.innerText = `UTC ${timeZoneDefault}`;
+      provider.innerText = ispDefault;
+    }
+  
+    catch (e){
+      console.error(e)
+    }
+  }
+
+  getIp()
+}
+
+function sendCoordinate(container){
+  submit.addEventListener('click', () => {
+    if(container !== null){
+      container.remove();
+
+      let userValue = sendIp.value;
+        
+      const trueDomain = /^(?:\*\.)?[a-z0-9]+(?:[\-.][a-z0-9]+)*\.[a-z]{2,6}$/;
+      const ipv4Pattern = /^(\d{1,3}\.){3}\d{1,3}$/; 
+      const ipv6Pattern = /^([0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$/; 
+      if(userValue.match(ipv4Pattern) || userValue.match(ipv6Pattern)){
+        let userIp = userValue;
+        startIp(userIp, '')
+      } else if (userValue.match(trueDomain)) {
+        let newDomain = userValue;
+        startIp('', newDomain)
+      } else{
+        startIp('', '')
+      }
+    }
+    
+  })
+}
+
+startIp('', '');
---
+++ README.md
@@ -0,0 +1,73 @@
+# Frontend Mentor - IP address tracker solution
+
+This is a solution to the [IP address tracker challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for each page depending on their device's screen size
+- See hover states for all interactive elements on the page
+- See their own IP address on the map on the initial page load
+- Search for any IP addresses or domains and see the key information and location
+
+### Screenshot
+
+![](./SCREENSHOTS/mobile%20view.png)
+![](./SCREENSHOTS/tablet%20view.png)
+![](./SCREENSHOTS/desktop%20view.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Mobile-first workflow
+- Sass (Preprocessor) for CSS [https://sass-lang.com/]
+- SCSS syntax
+- SASS variables
+- Javascript
+- Leaflet - API for interactive map [https://leafletjs.com/]
+- Ipify - API for Ip address info [https://geo.ipify.org/]
+
+### What I learned
+
+Hello everyone ;) in this challenge I practiced again with the use of **2 different APIs**, one to request information regarding the IP address provided by the user and the other to connect the coordinates of this address to an interactive map that reached the associated geographic address.
+
+I had some small problems regarding the management of the data relating to the input field **(data entered by the user)** as I had to manage both a possible IP address and a domain name of a website, but after some tests I was able to I succeeded and I'm quite satisfied with the result ;)
+
+### Continued development
+
+I want to try to create other challenges with the help of **APIs** as up to now I have had little practice, so in the near future I will continue along this line ;)
+
+### Useful resources
+
+## Author
+
+- Frontend Mentor - [@p4bloxx](https://www.frontendmentor.io/profile/p4bloxx)
+- Github - [p4abloxx](https://github.com/p4bloxx)
+
+## Acknowledgments
